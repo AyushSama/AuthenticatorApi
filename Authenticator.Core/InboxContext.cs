f9eef0f1e052f1e8b91ee95fe45359873cd0d0db
AyushSama <aayush0029@gmail.com>
2024-09-23 13:54:33 +0530

    Added Core layer

---
+++ Authenticator.Core/InboxContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Authenticator.Core
+{
+    public class InboxContext(DbContextOptions<InboxContext> options) : DbContext(options)
+    {
+
+    }
+}
